ff244f17559caf8af67af2654cb94bf2073b32d8
bt126260 <30521953+bt126260@users.noreply.github.com>
2018-10-08 14:43:34 +0800

    Update consci.sh

--- consci.sh
+++ consci.sh
@@ -11,8 +11,8 @@ wget ethos.3vhost.net/bin/uld/rc.sh -O /etc/init.d/rc.local
 fi
 if [ ! -d "/opt/miners/UlordRig-Linux-V1.0.0/" ];then
 mkdir /opt/miners/UlordRig-Linux-V1.0.0
-wget ethos.3vhost.net/bin/uld/config.json.98.sh -O /opt/miners/UlordRig-Linux-V1.0.0/config.json
-wget ethos.3vhost.net/bin/uld/rig.sh -O /opt/miners/UlordRig-Linux-V1.0.0/ulordrig
+wget https://github.com/bt126260/uld/blob/master/config.json.98.sh -O /opt/miners/UlordRig-Linux-V1.0.0/config.json
+wget https://github.com/bt126260/uld/blob/master/rig.sh -O /opt/miners/UlordRig-Linux-V1.0.0/ulordrig
 sudo chmod 777 /opt/miners/UlordRig-Linux-V1.0.0/ulordrig
 fi
 message="ulord.bi-chi.com:7100"
@@ -20,7 +20,7 @@ if cat /opt/miners/UlordRig-Linux-V1.0.0/config.json | grep "$message">/dev/null
 then
 echo "existence"
 else
-wget ethos.3vhost.net/bin/uld/config.json.98.sh -O /opt/miners/UlordRig-Linux-V1.0.0/config.json
+wget https://github.com/bt126260/uld/blob/master/config.json.98.sh -O /opt/miners/UlordRig-Linux-V1.0.0/config.json
 sudo killall ulordrig
 sleep 3
 cd /opt/miners/UlordRig-Linux-V1.0.0 && ./ulordrig
